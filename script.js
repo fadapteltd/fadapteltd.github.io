22b3d5d9638b72c84041c310ff3e3ccd59282786
fadapteltd <fadapteltd@gmail.com>
2025-10-05 02:35:15 +0800

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,34 @@
 function isMobile() {
-    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+	return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+		navigator.userAgent
+	);
 }
 
 function copyToClipboard(text) {
-    var textarea = document.createElement("textarea");
-    textarea.value = text;
-    document.body.appendChild(textarea);
-    textarea.select();
-    document.execCommand("copy");
-    document.body.removeChild(textarea);
+	var textarea = document.createElement("textarea");
+	textarea.value = text;
+	document.body.appendChild(textarea);
+	textarea.select();
+	document.execCommand("copy");
+	document.body.removeChild(textarea);
 }
 
-document.addEventListener("DOMContentLoaded", function() {
-    if (isMobile()) {
-        document.querySelector(".btn-phone").style.display = "inline-block";
-        var phoneButton = document.querySelector(".btn-phone");
-        phoneButton.addEventListener("click", function() {
-            var phoneNumber = "123-456-7890";
-            copyToClipboard(phoneNumber);
-            alert("电话号码已复制到剪贴板");
-        });
-    }
-    else{
-        document.querySelector(".btn-copy").style.display = "inline-block";
-        var copyButton = document.querySelector(".btn-copy");
-        copyButton.addEventListener("click", function() {
-            var phoneNumber = "123-456-7890";
-            copyToClipboard(phoneNumber);
-            alert("电话号码已复制到剪贴板");
-        })
-    }
+document.addEventListener("DOMContentLoaded", function () {
+	if (isMobile()) {
+		document.querySelector(".btn-phone").style.display = "inline-block";
+		var phoneButton = document.querySelector(".btn-phone");
+		phoneButton.addEventListener("click", function () {
+			var phoneNumber = "123-456-7890";
+			copyToClipboard(phoneNumber);
+			alert("电话号码已复制到剪贴板");
+		});
+	} else {
+		document.querySelector(".btn-copy").style.display = "inline-block";
+		var copyButton = document.querySelector(".btn-copy");
+		copyButton.addEventListener("click", function () {
+			var phoneNumber = "123-456-7890";
+			copyToClipboard(phoneNumber);
+			alert("电话号码已复制到剪贴板");
+		});
+	}
 });
